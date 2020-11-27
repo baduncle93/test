1b79730f992055ca774aac2a7eae3c2b7036738f
baduncle93 <38860973+baduncle93@users.noreply.github.com>
2020-11-27 14:15:04 +0100

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -5,10 +5,10 @@ exports.handler = function(event, context, callback) {
   console.log(event.queryStringParameters.first)
   console.log(event.queryStringParameters.second)
 
-  params = JSON.parse(event.queryStringParameters)
+  params = event.queryStringParameters
   console.log(params)
   callback(null, {
     statusCode: 200,
-    body: "Hello, Tom Turbo " + params.first + params.second
+    body: "Hello, Tom Turbo " + params.test + " " + params.first + params.second
   });
 };
