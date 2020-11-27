8ec6c7c5121253d30722eb12d00da1bacf9042a3
baduncle93 <38860973+baduncle93@users.noreply.github.com>
2020-11-27 13:26:47 +0100

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,8 @@
-const queryString = window.location.search;
-console.log(queryString);
+
 
 exports.handler = function(event, context, callback) {
+  const queryString = window.location.search;
+  console.log(queryString);
   callback(null, {
     statusCode: 200,
     body: "Hello, Tom Turbo " + queryString
