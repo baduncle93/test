984ca91d1a4885d517af7c7cefd4dad88d673323
baduncle93 <38860973+baduncle93@users.noreply.github.com>
2020-11-27 13:56:50 +0100

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -4,10 +4,10 @@ exports.handler = function(event, context, callback) {
 
   console.log(event);
   console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-  console.log(event.test);
-  console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-  console.log(event.url);
-  console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+  console.log(event.multiValueHeaders);
+  console.log("BBBBBBBBBBBBBBBBBBBBBBBBBBBBB")
+  console.log(event.multiValueHeaders.queryStringParameters);
+  console.log("CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC")
   callback(null, {
     statusCode: 200,
     body: "Hello, Tom Turbo " + event.headers
