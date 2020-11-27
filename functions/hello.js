58d54ab12c3dd6a902407f7c2de4514f15760cbc
baduncle93 <38860973+baduncle93@users.noreply.github.com>
2020-11-27 14:13:06 +0100

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -2,6 +2,9 @@
 
 exports.handler = function(event, context, callback) {
   console.log(event.queryStringParameters)
+  console.log(event.queryStringParameters.first)
+  console.log(event.queryStringParameters.second)
+
   params = JSON.parse(event.queryStringParameters)
   console.log(params)
   callback(null, {
