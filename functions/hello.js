ac9f93852905a9ea0672896f19d9ce85e18c70d8
baduncle93 <38860973+baduncle93@users.noreply.github.com>
2020-11-27 13:36:11 +0100

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -5,6 +5,6 @@ exports.handler = function(event, context, callback) {
   console.log(event);
   callback(null, {
     statusCode: 200,
-    body: "Hello, Tom Turbo " + event.APIGatewayProxyRequest +context
+    body: "Hello, Tom Turbo " + test
   });
 };
