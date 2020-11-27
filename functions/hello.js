4a87daf0fa8d4c07e8f5c25827f098aaafaa0356
baduncle93 <38860973+baduncle93@users.noreply.github.com>
2020-11-27 13:39:02 +0100

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -2,7 +2,7 @@
 
 exports.handler = function(event, context, callback) {
 
-  console.log(event);
+  console.log(event.headers);
   callback(null, {
     statusCode: 200,
     body: "Hello, Tom Turbo " + event.headers
